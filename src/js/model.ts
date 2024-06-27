7f56fb7558334c1c1448da1546ec86b87eee1f11
Interno <internuser04@gmail.com>
2024-06-27 10:17:15 -0300

    aniadido fullscreen funcional

--- src/js/model.ts
+++ src/js/model.ts
@@ -1,18 +1,19 @@
 // CON TYPESCRIPT
 
-import { Control, Map, TileLayer } from "leaflet";
+import { control, Map, TileLayer } from "leaflet";
+import "leaflet.fullscreen"
 
 const Model = {
     map: null as L.Map | null,
 
-    initializeMap(lat: number, lng: number, zoom:number):void {
+    initializeMap(lat: number, lng: number, zoom: number): void {
         this.map = new Map('map').setView([lat, lng], zoom);
 
         new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
         }).addTo(this.map);
 
-        new Control.Fullscreen().addTo(this.map);
+        control.fullscreen().addTo(this.map)
     },
 
 
